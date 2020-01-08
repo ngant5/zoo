a40c2a3248311aaf39320e713f1235bdefd63447
nga <nguyentuyetnga0209@gmail.com>
2020-01-08 15:45:58 +0700

    init client page

--- common/header-common.php
+++ common/header-common.php
@@ -2,18 +2,16 @@
     // include ("connection.php");
     function loop_array($array = array(), $parent_id = 0) {
         if (!empty($array[$parent_id])) { ?>
-
-            <ul id="navigation">
+            <ul id="navigation" class="nav-zoo row">
             <?php
             foreach($array[$parent_id] as $item) {
                 ?>
-
-                <li>
-                    <a href="<?=$item['id']?>"><?=$item['cate_name']?></a>
+                <li >
+                    <a href="<?="http://localhost/zoo/page/content.php?id={$item["id"]}" ?>"><?=$item["cate_name"]?></a>
                     <?php
                 while (loop_array($array, $item['id'])) { ?>
                 <li>
-                    <a href="<?=$item['id']?>"><?=$item['cate_name']?></a>
+                    <a href="<?="http://localhost/zoo/page/content.php?id={$item["id"]}" ?>"><?=$item["cate_name"]?></a>
                 </li>
                 <?php
                 }
@@ -30,7 +28,6 @@
         $sql = "SELECT * FROM category";
         $query = mysqli_query($conn, $sql);
         $array = array();
-
         if (mysqli_num_rows($query)) {
             while ($row = mysqli_fetch_array($query)) {
                 $array[$row['parent_id']][] = $row;
@@ -56,7 +53,7 @@
 <script src="../asset/js/mobile.js"></script>
 <script src="../asset/js/popper.min.js"></script>
 
-<title>WINT ZOO</title>
+<title>ZOO</title>
 <style>
     .nav-zoo ul li {
         list-style:none;
@@ -68,19 +65,40 @@
     }
     .nav-zoo ul li:hover li{
         display: inline-block;
+        margin-top: 10px;
+        margin-left: 5px;
+    }
+    ul.nav-zoo {
+        display: inline-block;
+    }
+    #header ul#navigation li a {
+        padding: 0px 10px;
     }
 </style>
 
 <div id="header">
     <h1>
         <a href="http://localhost/zoo/index.php"><img style="width:150px;" src="http://localhost/zoo/logo.jpg" alt="Logo">
-            <span>Beautiful and Natural Animal Kingdom</span>
+            <span><b>Beautiful and Natural Amusement Park</b></span>
         </a>
     </h1>
-    <ul id="navigation" class="nav-zoo">
-    <?php
-        display_menu();
-    ?>
-    </ul>
+    <!-- <ul id="navigation" class="nav-zoo">
+    <li><a href='http://localhost/zoo/index.php'>Home</a></li> -->
+    
+    <div class="container">
+        <?php
+            echo '<ul id="navigation" class="nav-zoo">';
+            echo "<li><a href='http://localhost/zoo/page/experience.php'>Home</a></li>";
+            echo '<ul id="navigation" class="nav-zoo">';
+            echo "<li><a href='http://localhost/zoo/page/experience.php'>Experience</a></li>";
+            display_menu();
+            echo '<ul id="navigation" class="nav-zoo">';
+            echo "<li><a href='http://localhost/zoo/page/experience.php'>Information</a></li>";
+            echo '<ul id="navigation" class="nav-zoo">';
+            echo "<li><a href='http://localhost/zoo/page/experience.php'>Bussiness</a></li>";
+            
+        ?>
+    </div>
+
 </div>
 
--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="user-scalable=0, width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-	<title>Belle &amp; Carrie Rehabilitation Yoga Web Template</title>
+	<title>Zoo Nactional</title>
 	<link rel="stylesheet" type="text/css" href="./asset/css/style.css">
 	<link rel="stylesheet" type="text/css" href="./asset/css/mobile.css">
 	<script type='text/javascript' src='./asset/js/mobile.js'></script>
@@ -17,20 +17,59 @@
 		width: 100%;
 		height: auto;
 	}
+	.nav-img {
+		width: 100%;
+	}
+	/* .featured {
+	background:url('./nav-mother-and-child.png');
+	height:396px;
+	}
+	.featured div p {
+	color:#fff;
+	font-family:rokkittregular;
+	font-size:24px;
+	line-height:30px;
+	margin:0 0 0 78px;
+	padding:32px 0 0;
+	text-shadow:0 1px 1px #000;
+}
+	.featured div h2 {
+	color:#FFF;
+	font-family:rokkittbold;
+	font-size:36px;
+	font-weight:400;
+	line-height:24px;
+	margin:0 0 0 78px;
+	padding:28px 0 0;
+	text-shadow:0 1px 1px #000;
+}
+	.featured div h2 span {
+	color:#fff600;
+	font-size:48px;
+	line-height:30px;
+} */
+
 </style>
 <body>
 	<?php include "./common/header-common.php"; ?>
 		<div id="body" class="video-body">
-			<!-- <div id="tagline">
-				<h1>a</h1>
+			<div id="tagline">
+				<h1></h1>
 				<p>
-					For Better Health &amp; Flexibility
+					
 				</p>
-			</div> -->
-			<video class="video-fluid" autoplay muted loop id="myVideo">
+			</div>
+			<!-- <video class="video-fluid" autoplay muted loop id="myVideo">
 				<source src="https://mdbootstrap.com/img/video/Sail-Away.mp4" type="video/mp4" />
-			</video>
+			</video> -->
+			<img class="nav-img" src="./nav-mother-and-child.png" alt="">
+			<!-- <div class="featured">
+				<div>
+					<p>
+						As The Saying Goes.. <br> You Don’t Know What You’ve Got <br> ‘Till It’s Gone.
+					</p>
+					<h2>Plant A Tree. <br><br> Grow A Tree. <br><br> <span>Save The Future.</span></h2>
+				</div>
+			</div> -->
 		</div>
 	<?php include "./common/footer-common.php"; ?>
-</body>
-</html>
