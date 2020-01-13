1738132083abbe6797e2c420bdcd2800bfb39b24
nga <nguyentuyetnga0209@gmail.com>
2020-01-13 11:01:17 +0700

    changed bg color

--- admin/index.php
+++ admin/index.php
@@ -37,7 +37,7 @@ $id = $_SESSION['user']['id'];
 
   <nav class="navbar navbar-expand navbar-dark bg-dark static-top">
 
-    <a class="navbar-brand mr-1" href="index.php">Zoo Planet- Admin</a>
+    <a class="navbar-brand mr-1" href="index.php">Zoo Planet - Admin</a>
 
     <button class="btn btn-link btn-sm text-white order-1 order-sm-0" id="sidebarToggle" href="#">
       <i class="fas fa-bars"></i>
