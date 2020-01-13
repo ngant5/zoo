fea13fe5bf2a726fcfa434ab110221f6303590e1
nga <nguyentuyetnga0209@gmail.com>
2020-01-13 10:50:10 +0700

    update search

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -32,7 +32,6 @@ $id = $_SESSION['user']['id'];
       <i class="fas fa-bars"></i>
     </button>
     <form class="d-none d-md-inline-block form-inline ml-auto mr-0 mr-md-3 my-2 my-md-0">
-      </div>
     </form>
     <!-- Navbar -->
     <ul class="navbar-nav ml-auto ml-md-0">
