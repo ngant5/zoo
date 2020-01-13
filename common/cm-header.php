baf7d71e5277cdd22ae751590f21b6b09a8c43f9
nga <nguyentuyetnga0209@gmail.com>
2020-01-13 17:00:35 +0700

    update cm-header

--- common/cm-header.php
+++ common/cm-header.php
@@ -41,12 +41,15 @@
 <link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<meta name="keywords" content="Zoo Planet Responsive web template, Bootstrap Web Templates, Flat Web Templates, Andriod Compatible web template, 
+<meta name="keywords" content="Zoo Planet Responsive web template, Bootstrap Web Templates, Flat Web Templates, Andriod Compatible web template,
 Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyErricsson, Motorola web design" />
 <script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
 <script src="js/jquery-1.11.1.min.js"></script>
 <script src="js/bootstrap.js"></script>
 <style>
+.header-top {
+    padding: 0px !important;
+}
     .navbar-nav li {
         position: relative;
         display: inline-block !important;
@@ -55,8 +58,8 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
     .navbar-nav li li {
         display: none !important;
     }
-    .navbar-nav li li a{
-        padding: 2px  !important;
+    .navbar-nav li li a {
+        padding: 1px!important;
     }
     .navbar-nav li:hover li{
         display: inline-block !important;
@@ -92,7 +95,6 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
                     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                         <ul class="nav navbar-nav">
                             <?php
-                            
                             echo "<ul class='nav navbar-nav'><li><a href='http://localhost/zoo/about-us.php'>Aubout Us</a></li></ul>";
                             echo "<ul class='nav navbar-nav'><li><a href='http://localhost/zoo/whats-up.php'>Whats Up</a></li></ul>";
                             display_menu();
